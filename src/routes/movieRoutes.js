7139196d71b727c3e5c926e1756b61ca15bc3427
Josephnp732 <josephnp732@gmail.com>
2020-05-07 15:38:46 -0400

    Added Comment

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -2,7 +2,7 @@ import { getAllMovies } from '../controllers/movieController';
 import { getAllActors } from '../controllers/actorController';
 import { getAllCharacters } from '../controllers/characterController';
 
-
+// Funtion to get All data of all APIs
 const movieRoute = (app) => {
     app.route('/movies/all')
         .get(getAllMovies);
