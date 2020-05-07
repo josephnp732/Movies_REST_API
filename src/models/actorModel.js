e9900482869cb22c323238e327e5621579b03b89
Josephnp732 <josephnp732@gmail.com>
2020-05-07 12:32:17 -0400

    Added new Configurations and Models

---
+++ src/models/actorModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+export const Actor = new mongoose.Schema({
+    actorid: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    date_of_birth: {
+        type: String,
+    },
+    birth_city: {
+        type: String,
+    },
+    birth_country: {
+        type: String,
+    },
+    height_inches: {
+        type: String,
+    },
+    biography: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    },
+    ethnicity: {
+        type: String,
+    },
+    networth: {
+        type: String,
+    }
+});
---
+++ src/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String
+    },   
+    hashPassword: {
+        type: String,
+        required: true
+    },
+    created_date: {
+        type: String,
+        default: Date.now
+    }
+});
+
+// Compare Password with HashPassword
+UserSchema.methods.comparePassword = (password, hashPassword) => {
+    return bcrypt.compareSync(password, hashPassword);
+}
